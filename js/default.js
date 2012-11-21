a26fb96981b97aabf0cd161e654320ac96f4ba44
Lelouch <LelouchHe@gmail.com>
2012-11-21 12:18:27 +0800

    add code lineno

--- js/default.js
+++ js/default.js
@@ -5,5 +5,5 @@ bodies[0].setAttribute("onload", "prettyPrint()");
 var pres = document.getElementsByTagName("pre");
 for (var i = 0; i < pres.length; i++)
 {
-    pres[i].setAttribute("class", "prettyprint");
+    pres[i].setAttribute("class", "prettyprint linenums");
 }
